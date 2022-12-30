2e05d178f3db69e90f36e9042d1e7604cc14d0c1
Jose Luis Junior Gutierrez Aguero <jlgutierrez2000@hotmail.com>
2022-12-30 18:09:46 -0300

    Java Spring dia 1

---
+++ Java Spring/HolaSpring/src/main/java/com/juniorgutierrez/HolaSpring/HolaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.juniorgutierrez.HolaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaSpringApplication.class, args);
+	}
+
+}
