b8a47cb7b6a2bee062f9a456ddb9a995492cb842
Jose Luis Junior Gutierrez Aguero <jlgutierrez2000@hotmail.com>
2023-02-25 14:36:11 -0300

    GraphQL

---
+++ Platzi/Curso de GraphQL con Node.js/curso-nodejs-graphql/src/graphql/index.js
@@ -0,0 +1,29 @@
+const { ApolloServer } = require('apollo-server-express')
+const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core')
+
+const typeDefs = `
+  type Query {
+    hello: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: () => 'hola mundo'
+  }
+}
+
+const useGraphql = async (app) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    playground: true,
+    plugins: [
+      ApolloServerPluginLandingPageGraphQLPlayground
+    ]
+  })
+  await server.start()
+  server.applyMiddleware({ app })
+}
+
+module.exports = useGraphql
